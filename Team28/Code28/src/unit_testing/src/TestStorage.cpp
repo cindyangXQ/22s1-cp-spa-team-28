6be153b8defe5c8e707e0ca2f4d97f282d40fe02
luayida99 <77190041+luayida99@users.noreply.github.com>
2022-08-30 17:37:22 +0800

    Failing unit test

--- Team28/Code28/src/unit_testing/src/TestStorage.cpp
+++ Team28/Code28/src/unit_testing/src/TestStorage.cpp
@@ -4,4 +4,10 @@
 
 TEST_CASE("Storage is initialised correctly") {
 	Storage<TableValue> storage;
+	NamesTable<ConstantName, Constant>* constants = (NamesTable<ConstantName, Constant>*) storage.getTable(TableName::CONSTANTS);
+	Constant test = Constant("Test");
+
+	constants->store(&test);
+
+	REQUIRE(constants->retrieve(test.getName()) == test);
 }
