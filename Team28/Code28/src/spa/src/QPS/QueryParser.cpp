0655669c0f5a3798cbf39609c89b5a2749d428ce
Galnerd <e0543848@u.nus.edu>
2022-09-02 01:18:44 +0800

    Parser can now parse such that clauses

--- Team28/Code28/src/spa/src/QPS/QueryParser.cpp
+++ Team28/Code28/src/spa/src/QPS/QueryParser.cpp
@@ -1,10 +1,8 @@
 #include "QueryParser.h"
-#include "Utils.h"
-#include "./Error/ParseError.h"
 
 SolvableQuery QueryParser::parse(std::string query) {
     std::vector<std::string> clauses = Utils::splitString(query, ';');
-    int size = clauses.size();
+    size_t size = clauses.size();
 
     // Extract main clause
     std::string mainClause = Utils::removeTrailingSpaces(clauses[size - 1]);
@@ -14,7 +12,7 @@ SolvableQuery QueryParser::parse(std::string query) {
 
     Declaration decl = QueryParser::parseDeclaration(clauses);
     SelectClause selectCl = QueryParser::parseSelectClause(mainClause, decl.syns);
-    SuchThatClause suchThatCl = QueryParser::parseSuchThatClause(mainClause);
+    SuchThatClause suchThatCl = QueryParser::parseSuchThatClause(mainClause, decl.syns);
     PatternClause patternCl = QueryParser::parsePatternClause(mainClause);
 
     return SolvableQuery(decl, selectCl, suchThatCl, patternCl);
@@ -89,19 +87,79 @@ SelectClause QueryParser::parseSelectClause(std::string mainClause, std::vector<
     }
 }
 
-SuchThatClause QueryParser::parseSuchThatClause(std::string mainClause) {
+bool isSuchThatClause(std::vector<std::string> tokens, size_t start) {
+    if (start > tokens.size() - 8) {
+        return false;
+    }
+    std::vector<std::string> relref{"Modifies", "Uses", "Parent", "Parent*", "Follows", "Follows*"};
+    if (tokens[start] == "such" && tokens[start + 1] == "that") {
+        if (Utils::in(relref, tokens[start + 2]) && 
+            tokens[start + 3] == "(" && 
+            tokens[start + 5] == "," && 
+            tokens[start + 7] == ")") {
+            return true;
+        }
+        else {
+            throw ParseError("Syntax error for such that clause");
+        }
+    }
+    return false;
+}
+
+RelRef getRelationshipReference(std::string input) {
+    RelRef r = RelRef::Empty;
+    if (input.compare("Modifies") == 0) {
+        r = RelRef::Modifies;
+    }
+    else if (input.compare("Uses") == 0) {
+        r = RelRef::Uses;
+    }
+    else if (input.compare("Parent") == 0) {
+        r = RelRef::Parent;
+    }
+    else if (input.compare("Parent*") == 0) {
+        r = RelRef::ParentT;
+    }
+    else if (input.compare("Follows") == 0) {
+        r = RelRef::Follows;
+    }
+    else if (input.compare("Follows*") == 0) {
+        r = RelRef::FollowsT;
+    }
+    return r;
+}
+Reference getReference(std::string input, std::vector<Synonym> syns) {
+    if (std::all_of(input.begin(), input.end(), ::isdigit)) {
+        return StatementNumber(atoi(input.c_str()));
+    }
+    if (input[0] == '"' && input[-1] == '"') {
+        return VariableName(input);
+    }
+    Synonym selectedSyn;
+    bool isSynInit = false;
+    for (int i = 0; i < syns.size(); i++) {
+        Synonym s = syns[i];
+        if (input.compare(s.name) == 0) {
+            selectedSyn = s;
+            isSynInit = true;
+            return selectedSyn;
+        }
+    }
+    throw ParseError("not a number, not a name, not a synonym declared");
+}
+SuchThatClause QueryParser::parseSuchThatClause(std::string mainClause, std::vector<Synonym> syns) {
+    std::vector<char> special_char{ ';', '(', ',', ')', '_' };
+    std::vector<std::string> tokens = Utils::tokenize(mainClause, special_char);
+    size_t i;
+    for (i = 0; i < tokens.size(); i++) {
+        if (isSuchThatClause(tokens, i)) {
+            RelRef r = getRelationshipReference(tokens[i]);
+            Reference left = getReference(tokens[i + 4], syns);
+            Reference right = getReference(tokens[i + 6], syns);
+            return SuchThatClause(r, left, right);
+        }
+    }
     return SuchThatClause();
-    /*
-    size_t suchThatIdx = mainClause.find("such that");
-    size_t patternIdx = mainClause.find("pattern");
-    if (suchThatIdx < mainClause.size()) {
-        std::string suchThatString = mainClause.substr(suchThatIdx, std::min(patternIdx, mainClause.size()) - suchThatIdx);
-        return SuchThatClause(suchThatString);
-    }
-    else {
-        return SuchThatClause();
-    }
-    */
 }
 
 PatternClause QueryParser::parsePatternClause(std::string mainClause) {
