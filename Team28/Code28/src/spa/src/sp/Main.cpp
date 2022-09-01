1820af90f1e8b551654dfe2be694a34b85fb0441
cindyangXQ <yangc2105@gmail.com>
2022-09-02 03:48:20 +0800

    Update Main.cpp

--- Team28/Code28/src/spa/src/sp/Main.cpp
+++ Team28/Code28/src/spa/src/sp/Main.cpp
@@ -1,4 +1,6 @@
 #include "Tokenizer.h"
+#include "SimpleParser.h"
+#include "Entity.h"
 #include <iostream>
 
 using namespace std;
@@ -7,6 +9,8 @@ int main() {
 	string sourceProgram = "procedure Bedok {\nwest = 9 + east;\ny = east - 4;\nz = west + 2;\nwest\n= 9 + east + west;\n}";
 	Tokenizer tokenizer = Tokenizer(sourceProgram);
 	vector<Token> token_list = tokenizer.tokenize();
+	ProgramParser parser = ProgramParser();
+	Program program = parser.parse(token_list).entity;
 
 	for (int i = 0; i < token_list.size(); i++) {
 		Token temp = token_list[i];
