1d6eac2e9d274c2221974a3862d4432277ff760d
benedictchuajj <benedictchuajj@gmail.com>
2022-09-01 15:32:07 +0800

    Fix issues where Tables retrieved could not store items

--- Team28/Code28/src/spa/src/PKB/Storage.h
+++ Team28/Code28/src/spa/src/PKB/Storage.h
@@ -3,6 +3,7 @@
 #include <map>
 #include <string>
 #include <utility>
+#include <iostream>
 
 #include "Entity.h"
 #include "Table.h"
@@ -21,25 +22,28 @@ enum class TableName {
 	CONSTANTS
 };
 
-template<typename T>
+typedef NamesTable<ProcedureName, Procedure> ProceduresTable;
+typedef NamesTable<VariableName, Variable> VariablesTable;
+typedef NamesTable<ConstantName, Constant> ConstantsTable;
+
 class Storage {
 public:
 	explicit Storage() {
-		StatementsTable statements;
-		NamesTable<ProcedureName, Procedure> procedures;
-		NamesTable<VariableName, Variable> variables;
-		NamesTable<ConstantName, Constant> constants;
+		StatementsTable* statements = new StatementsTable();
+		ProceduresTable* procedures = new ProceduresTable();
+		VariablesTable* variables = new VariablesTable();
+		ConstantsTable* constants = new ConstantsTable();
 
-		this->tables[TableName::STATEMENTS] = (Table<T>*) &statements;
-		this->tables[TableName::PROCEDURES] = (Table<T>*) &procedures;
-		this->tables[TableName::VARIABLES] = (Table<T>*) &variables;
-		this->tables[TableName::CONSTANTS] = (Table<T>*) &constants;
+		this->tables[TableName::STATEMENTS] = (Table<TableValue>*) statements;
+		this->tables[TableName::PROCEDURES] = (Table<TableValue>*) procedures;
+		this->tables[TableName::VARIABLES] = (Table<TableValue>*) variables;
+		this->tables[TableName::CONSTANTS] = (Table<TableValue>*) constants;
 	};
 
-	Table<T>* getTable(TableName name) {
+	Table<TableValue>* getTable(TableName name) {
 		return this->tables.at(name);
 	};
 
 private:
-	std::map<TableName, Table<T>*> tables;
+	std::map<TableName, Table<TableValue>*> tables;
 };
--- Team28/Code28/src/unit_testing/src/TestStorage.cpp
+++ Team28/Code28/src/unit_testing/src/TestStorage.cpp
@@ -2,11 +2,12 @@
 
 #include "catch.hpp"
 
+#include <iostream>
 TEST_CASE("Storage is initialised correctly") {
-	Storage<TableValue> storage;
-	NamesTable<ConstantName, Constant>* constants = (NamesTable<ConstantName, Constant>*) storage.getTable(TableName::CONSTANTS);
-	NamesTable<VariableName, Variable>* variables = (NamesTable<VariableName, Variable>*) storage.getTable(TableName::VARIABLES);
-	NamesTable<ProcedureName, Procedure>* procedures = (NamesTable<ProcedureName, Procedure>*) storage.getTable(TableName::PROCEDURES);
+	Storage storage;
+	ConstantsTable* constants = (ConstantsTable*) storage.getTable(TableName::CONSTANTS);
+	VariablesTable* variables = (VariablesTable*) storage.getTable(TableName::VARIABLES);
+	ProceduresTable* procedures = (ProceduresTable*) storage.getTable(TableName::PROCEDURES);
 	StatementsTable* statements = (StatementsTable*)storage.getTable(TableName::STATEMENTS);
 
 	REQUIRE(constants != nullptr);
@@ -16,12 +17,26 @@ TEST_CASE("Storage is initialised correctly") {
 
 }
 
-TEST_CASE("Storage stores and retrieves correctly") {
-	Storage<TableValue> storage;
-	NamesTable<ConstantName, Constant>* constants = (NamesTable<ConstantName, Constant>*) storage.getTable(TableName::CONSTANTS);
+TEST_CASE("Storage stores and retrieves Constants correctly") {
+	Storage storage;
+	ConstantsTable* constants = (ConstantsTable*) storage.getTable(TableName::CONSTANTS);
 	Constant test = Constant("Test");
+	
+	REQUIRE(constants->getTableSize() == 0);
+	constants->store(&test);
 
-	// constants->store(&test);
+	REQUIRE(constants->getTableSize() == 1);
+	REQUIRE(*constants->retrieve(test.getName()) == test);
+}
 
-	// REQUIRE(constants->getTableSize() == 1);
+TEST_CASE("Storage stores and retrieves Statements correctly") {
+	Storage storage;
+	StatementsTable* statements = (StatementsTable*) storage.getTable(TableName::STATEMENTS);
+	REQUIRE(statements->getTableSize() == 0);
+
+	Statement test = Statement(1, StatementType::ASSIGN);
+	statements->store(&test);
+
+	REQUIRE(statements->getTableSize() == 1);
+	REQUIRE(*statements->retrieve(test.getIndex()) == test);
 }
