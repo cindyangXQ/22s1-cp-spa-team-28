b911690c80cb7b855f3c8f4354a7b12d465ef662
luayida99 <77190041+luayida99@users.noreply.github.com>
2022-09-04 23:39:31 +0800

    Add SPA wrapper class

---
+++ Team28/Code28/src/spa/src/SPA.cpp
@@ -0,0 +1,15 @@
+#include "SPA.h"
+
+SPA::SPA() {
+	this->pkb = PKB();
+	this->qps = QPS(this->pkb.queryFacade);
+	this->sp = SP(this->pkb.populateFacade);
+}
+
+void SPA::parse(std::string filename) {
+	this->sp.parse(filename);
+}
+
+void SPA::evaluateQuery(std::string query, std::list<std::string>& results) {
+	this->qps.evaluate(query, results);
+}
