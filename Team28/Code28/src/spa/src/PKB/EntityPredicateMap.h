1ac9bef0419633417c8a1c3c49c4346d2e6d5e8f
benedictchuajj <benedictchuajj@gmail.com>
2022-09-09 15:42:21 +0800

    Refactor EntityPredicateMap

--- Team28/Code28/src/spa/src/PKB/EntityPredicateMap.h
+++ Team28/Code28/src/spa/src/PKB/EntityPredicateMap.h
@@ -10,92 +10,43 @@
 
 /*
 * Classes representing a PredicateMap for filtering NamesTable.
-* TODO: make everything into a proper template class EntityPredicateMap if possible
 */
 template <typename T>
 class EntityPredicateMap : public PredicateMap<EntityHeader, T> {
 public:
-	virtual bool isEmpty() = 0;
-	// virtual std::map<EntityHeader, Entity<Name>*> getPredicateMap() = 0; 
+	EntityPredicateMap(std::map<EntityHeader, T*> *predicateMap) {
+		this->predicateMap = *predicateMap;
+	};
+	
+	/*
+	* Checks if PredicateMap is empty.
+	*/
+    bool isEmpty() {
+		return this->predicateMap.size() == 0;
+	};
+
+	/*
+	* Gets the map of the predicate.
+	*/
+    std::map<EntityHeader, T*> getPredicateMap() {
+		return this->predicateMap;
+	};
+
+	protected:
+		std::map<EntityHeader, T*> predicateMap;
 };
 
 class ConstantPredicateMap : public EntityPredicateMap<Constant> {
 public:
-	/*
-	* Explicit constructor for a PredicateMap.
-	*/
-	explicit ConstantPredicateMap(std::map<EntityHeader, Constant*> *predicateMap) {
-		this->predicateMap = *predicateMap;
-	};
-	
-	/*
-	* Checks if PredicateMap is empty.
-	*/
-    bool isEmpty() {
-		return this->predicateMap.size() == 0;
-	};
-
-	/*
-	* Gets the map of the predicate.
-	*/
-    std::map<EntityHeader, Constant*> getPredicateMap() {
-		return this->predicateMap;
-	};
-    
-    private:
-        std::map<EntityHeader, Constant*> predicateMap;
+	explicit ConstantPredicateMap(std::map<EntityHeader, Constant*> *predicateMap) : EntityPredicateMap(predicateMap) {};
 };
 
 class VariablePredicateMap : public EntityPredicateMap<Variable> {
 public:
-	/*
-	* Explicit constructor for a PredicateMap.
-	*/
-	explicit VariablePredicateMap(std::map<EntityHeader, Variable*> *predicateMap) {
-		this->predicateMap = *predicateMap;
-	};
-	
-	/*
-	* Checks if PredicateMap is empty.
-	*/
-    bool isEmpty() {
-		return this->predicateMap.size() == 0;
-	};
-
-	/*
-	* Gets the map of the predicate.
-	*/
-    std::map<EntityHeader, Variable*> getPredicateMap() {
-		return this->predicateMap;
-	};
-    
-    private:
-        std::map<EntityHeader, Variable*> predicateMap;
+	explicit VariablePredicateMap(std::map<EntityHeader, Variable*> *predicateMap) : EntityPredicateMap(predicateMap) {};
 };
 
 class ProcedurePredicateMap : public EntityPredicateMap<Procedure> {
 public:
-	/*
-	* Explicit constructor for a PredicateMap.
-	*/
-	explicit ProcedurePredicateMap(std::map<EntityHeader, Procedure*> *predicateMap) {
-		this->predicateMap = *predicateMap;
-	};
-	
-	/*
-	* Checks if PredicateMap is empty.
-	*/
-    bool isEmpty() {
-		return this->predicateMap.size() == 0;
-	};
-
-	/*
-	* Gets the map of the predicate.
-	*/
-    std::map<EntityHeader, Procedure*> getPredicateMap() {
-		return this->predicateMap;
-	};
-    
-    private:
-        std::map<EntityHeader, Procedure*> predicateMap;
+	explicit ProcedurePredicateMap(std::map<EntityHeader, Procedure*> *predicateMap) : EntityPredicateMap(predicateMap) {};
 };
