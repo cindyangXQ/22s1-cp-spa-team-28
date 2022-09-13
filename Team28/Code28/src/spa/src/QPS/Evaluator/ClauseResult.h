c193d865f3e787c9cff3b018a160980695e4bf2b
CSYGalvin <csygalvin@gmail.com>
2022-09-13 18:43:18 +0800

    Insert result from pkb into a clauseResult object for later processing

--- Team28/Code28/src/spa/src/QPS/Evaluator/ClauseResult.h
+++ Team28/Code28/src/spa/src/QPS/Evaluator/ClauseResult.h
@@ -3,10 +3,15 @@
 class ClauseResult {
 public:
     ClauseTable table;
-    bool isEmpty;
+    bool isEmpty = true;
     ClauseResult();
     ClauseResult(bool empty) {
         this->isEmpty = empty;
     }
     explicit ClauseResult(std::vector<Synonym> headers) : table(ClauseTable(headers)) {};
+
+    void insert(Tuple t) {
+        this->isEmpty = false;
+        this->table.insert(t);
+    }
 };
