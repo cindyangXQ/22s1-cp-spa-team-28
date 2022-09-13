e1d93729fa66997f779dbbead6350b8ac8c71625
Galnerd <e0543848@u.nus.edu>
2022-09-13 12:42:33 +0800

    Remove assert

--- Team28/Code28/src/spa/src/QPS/Evaluator/ClauseTable.cpp
+++ Team28/Code28/src/spa/src/QPS/Evaluator/ClauseTable.cpp
@@ -18,7 +18,6 @@ std::vector<int> ClauseTable::getIndices(std::vector<Synonym> common_headers) {
 }
 
 void ClauseTable::insert(Tuple row) {
-	_ASSERT(row.size() == this->header.size());
 	this->rows.push_back(row);
 }
 
