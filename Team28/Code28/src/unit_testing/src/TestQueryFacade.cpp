922474809b5861f8a2d729ca10848b64abaa7dc2
benedictchuajj <benedictchuajj@gmail.com>
2022-09-15 00:59:44 +0800

    Add unit tests for QueryFacade -  Modifies and Uses

--- Team28/Code28/src/unit_testing/src/TestQueryFacade.cpp
+++ Team28/Code28/src/unit_testing/src/TestQueryFacade.cpp
@@ -140,13 +140,13 @@ TEST_CASE("StmtToStmt: Validate returns correct results") {
 	Statement line1 = Statement(1, StatementType::ASSIGN);
 	Statement line2 = Statement(2, StatementType::ASSIGN);
 	Relationship<int, int> rs = Relationship(RelationshipReference::FOLLOWS, 1, 2);
-	Reference leftRef = Reference("1");
-	Reference rightRef = Reference("2");
-	Reference wildcardRef = Reference("_");
-
 	statements->store(&line1);
 	statements->store(&line2);
 	follows->store(&rs);
+
+	Reference leftRef = Reference("1");
+	Reference rightRef = Reference("2");
+	Reference wildcardRef = Reference("_");
 
 	// Queries where relationships do exist
 	REQUIRE(facade.validate(RelationshipReference::FOLLOWS, leftRef, rightRef));
@@ -207,7 +207,6 @@ TEST_CASE("StmtToStmt: SolveRight queries for Follows(1, 2) return correct resul
 	output = facade.solveRight(RelationshipReference::FOLLOWS, leftRef, rightEntityName);
 	REQUIRE(std::equal(expectedResult.begin(), expectedResult.end(), output.begin()));
 }
-
 
 TEST_CASE("StmtToStmt: SolveLeft queries for Follows(1, 2) return correct results") {
 	Storage storage;
@@ -301,23 +300,364 @@ TEST_CASE("StmtToStmt: SolveBoth queries for Follows(1, 2) return correct result
 	REQUIRE(std::equal(expectedResult.begin(), expectedResult.end(), output.begin()));
 }
 
-// TEST_CASE("StmtToVar: Validate returns correct results") {
-// 	Storage storage;
-// 	QueryFacade facade = QueryFacade(&storage);
-// 	StatementsTable* statements = (StatementsTable*)storage.getTable(TableName::STATEMENTS);
-// 	ModifiesSTable* follows = (ModifiesSTable*)storage.getTable(TableName::MODIFIES_S);
+TEST_CASE("StmtToVar: Validate returns correct results") {
+	Storage storage;
+	QueryFacade facade = QueryFacade(&storage);
+	StatementsTable* statements = (StatementsTable*)storage.getTable(TableName::STATEMENTS);
+	VariablesTable* variables = (VariablesTable*)storage.getTable(TableName::VARIABLES);
+	ModifiesSTable* modifiesS = (ModifiesSTable*)storage.getTable(TableName::MODIFIES_S);
 	
-// 	Statement line1 = Statement(1, StatementType::ASSIGN);
-// 	Relationship<int, std::string> rs = Relationship(
-// 		RelationshipReference::MODIFIES, 1, std::string("a")
-// 	);
-// 	Reference leftRef = Reference("1");
-// 	Reference rightRef = Reference("a");
+	Statement line1 = Statement(1, StatementType::ASSIGN);
+	Variable var1 = Variable("a");
+	Relationship<int, std::string> rs = Relationship(
+		RelationshipReference::MODIFIES, 1, std::string("a")
+	);
+	statements->store(&line1);
+	variables->store(&var1);
+	modifiesS->store(&rs);
 
-// 	statements->store(&line1);
-// 	follows->store(&rs);
+	Reference leftRef = Reference("1");
+	Reference rightRef = Reference("a");
+	Reference wildcardRef = Reference("_");
 
-// 	REQUIRE(facade.validate(RelationshipReference::MODIFIES, leftRef, rightRef));
-// 	REQUIRE(!facade.validate(RelationshipReference::MODIFIES, Reference("2"), rightRef));
-// 	REQUIRE(!facade.validate(RelationshipReference::MODIFIES, leftRef, Reference("x")));
-// }
+	// Queries where relationships do exist
+	REQUIRE(facade.validate(RelationshipReference::MODIFIES, leftRef, rightRef));
+	REQUIRE(facade.validate(RelationshipReference::MODIFIES, leftRef, wildcardRef));
+	REQUIRE(facade.validate(RelationshipReference::MODIFIES, wildcardRef, rightRef));
+	REQUIRE(facade.validate(RelationshipReference::MODIFIES, wildcardRef, wildcardRef));
+
+	// Queries where relationships do not exist
+	REQUIRE(!facade.validate(RelationshipReference::MODIFIES, Reference("2"), rightRef));
+	REQUIRE(!facade.validate(RelationshipReference::MODIFIES, leftRef, Reference("x")));
+	REQUIRE(!facade.validate(RelationshipReference::MODIFIES, Reference("2"), wildcardRef));
+	REQUIRE(!facade.validate(RelationshipReference::MODIFIES, wildcardRef, Reference("x")));
+}
+
+TEST_CASE("StmtToVar: SolveRight queries for Modifies(1, 'a') return correct results") {
+	Storage storage;
+	QueryFacade facade = QueryFacade(&storage);
+	StatementsTable* statements = (StatementsTable*)storage.getTable(TableName::STATEMENTS);
+	VariablesTable* variables = (VariablesTable*)storage.getTable(TableName::VARIABLES);
+	ModifiesSTable* modifiesS = (ModifiesSTable*)storage.getTable(TableName::MODIFIES_S);
+	
+	Statement line1 = Statement(1, StatementType::ASSIGN);
+	Variable var1 = Variable("a");
+	Relationship<int, std::string> rs = Relationship(
+		RelationshipReference::MODIFIES, 1, std::string("a")
+	);
+	statements->store(&line1);
+	variables->store(&var1);
+	modifiesS->store(&rs);
+
+	Reference leftRef;
+	EntityName rightEntityName;
+	std::vector<Value> expectedResult;
+	std::vector<Value> output;
+	
+	// SolveRight(Modifies, 1, Variable) for Modifies(1, 'a') returns {'a'}
+	leftRef = Reference("1");
+	rightEntityName = EntityName::VARIABLE;
+	expectedResult = {Value(ValueType::VAR_NAME, "a")};
+	output = facade.solveRight(RelationshipReference::MODIFIES, leftRef, rightEntityName);
+	REQUIRE(std::equal(expectedResult.begin(), expectedResult.end(), output.begin()));
+
+	// SolveRight(Modifies, 2, Variable) for Modifies(1, 'a') returns {}
+	leftRef = Reference("2");
+	rightEntityName = EntityName::VARIABLE;
+	expectedResult = {};
+	output = facade.solveRight(RelationshipReference::MODIFIES, leftRef, rightEntityName);
+	REQUIRE(std::equal(expectedResult.begin(), expectedResult.end(), output.begin()));
+
+	// SolveRight(Modifies, 1, Procedure) for Modifies(1, 'a') returns {}
+	leftRef = Reference("1");
+	rightEntityName = EntityName::PROCEDURE;
+	expectedResult = {};
+	output = facade.solveRight(RelationshipReference::MODIFIES, leftRef, rightEntityName);
+	REQUIRE(std::equal(expectedResult.begin(), expectedResult.end(), output.begin()));
+
+	// SolveRight(Modifies, _, Variable) for Modifies(1, 'a') returns {'a'}
+	leftRef = Reference("_");
+	rightEntityName = EntityName::VARIABLE;
+	expectedResult = {Value(ValueType::VAR_NAME, "a")};
+	output = facade.solveRight(RelationshipReference::MODIFIES, leftRef, rightEntityName);
+	REQUIRE(std::equal(expectedResult.begin(), expectedResult.end(), output.begin()));
+}
+
+TEST_CASE("StmtToVar: SolveLeft queries for Modifies(1, 'a') return correct results") {
+	Storage storage;
+	QueryFacade facade = QueryFacade(&storage);
+	StatementsTable* statements = (StatementsTable*)storage.getTable(TableName::STATEMENTS);
+	VariablesTable* variables = (VariablesTable*)storage.getTable(TableName::VARIABLES);
+	ModifiesSTable* modifiesS = (ModifiesSTable*)storage.getTable(TableName::MODIFIES_S);
+	
+	Statement line1 = Statement(1, StatementType::ASSIGN);
+	Variable var1 = Variable("a");
+	Relationship<int, std::string> rs = Relationship(
+		RelationshipReference::MODIFIES, 1, std::string("a")
+	);
+	statements->store(&line1);
+	variables->store(&var1);
+	modifiesS->store(&rs);
+
+	Reference rightRef;
+	EntityName leftEntityName;
+	std::vector<Value> expectedResult;
+	std::vector<Value> output;
+
+	// SolveLeft(Modifies, a, Assign) for Modifies(1, 'a') returns {'1'}
+	rightRef = Reference("a");
+	leftEntityName = EntityName::ASSIGN;
+	expectedResult = {Value(ValueType::STMT_NUM, "1")};
+	output = facade.solveLeft(RelationshipReference::MODIFIES, rightRef, leftEntityName);
+	REQUIRE(std::equal(expectedResult.begin(), expectedResult.end(), output.begin()));
+
+	// SolveLeft(Modifies, x, Assign) for Modifies(1, 'a') returns {}
+	rightRef = Reference("x");
+	leftEntityName = EntityName::ASSIGN;
+	expectedResult = {};
+	output = facade.solveLeft(RelationshipReference::MODIFIES, rightRef, leftEntityName);
+	REQUIRE(std::equal(expectedResult.begin(), expectedResult.end(), output.begin()));
+
+	// SolveLeft(Modifies, a, Print) for Modifies(1, 'a') returns {}
+	rightRef = Reference("a");
+	leftEntityName = EntityName::PRINT;
+	expectedResult = {};
+	output = facade.solveLeft(RelationshipReference::MODIFIES, rightRef, leftEntityName);
+	REQUIRE(std::equal(expectedResult.begin(), expectedResult.end(), output.begin()));
+
+	// SolveLeft(Modifies, _, Assign) for Modifies(1, 'a') returns {'1'}
+	rightRef = Reference("_");
+	leftEntityName = EntityName::ASSIGN;
+	expectedResult = {Value(ValueType::STMT_NUM, "1")};
+	output = facade.solveLeft(RelationshipReference::MODIFIES, rightRef, leftEntityName);
+	REQUIRE(std::equal(expectedResult.begin(), expectedResult.end(), output.begin()));
+
+}
+
+TEST_CASE("StmtToVar: SolveBoth queries for Modifies(1, 'a') return correct results") {
+	Storage storage;
+	QueryFacade facade = QueryFacade(&storage);
+	StatementsTable* statements = (StatementsTable*)storage.getTable(TableName::STATEMENTS);
+	VariablesTable* variables = (VariablesTable*)storage.getTable(TableName::VARIABLES);
+	ModifiesSTable* modifiesS = (ModifiesSTable*)storage.getTable(TableName::MODIFIES_S);
+	
+	Statement line1 = Statement(1, StatementType::ASSIGN);
+	Variable var1 = Variable("a");
+	Relationship<int, std::string> rs = Relationship(
+		RelationshipReference::MODIFIES, 1, std::string("a")
+	);
+	statements->store(&line1);
+	variables->store(&var1);
+	modifiesS->store(&rs);
+	Value value1 = Value(ValueType::STMT_NUM, "1");
+	Value value2 = Value(ValueType::VAR_NAME, "a");
+
+	EntityName leftEntityName;
+	EntityName rightEntityName;
+	std::vector<std::pair<Value,Value>> expectedResult;
+	std::vector<std::pair<Value,Value>> output;
+
+	// SolveBoth(Modifies, Stmt, Var) for Modifies(1, 'a') returns {('1', 'a')}
+	leftEntityName = EntityName::STMT;
+	rightEntityName = EntityName::VARIABLE;
+	expectedResult = {std::make_pair(value1, value2)};
+	output = facade.solveBoth(RelationshipReference::MODIFIES, leftEntityName, rightEntityName);
+	REQUIRE(expectedResult.size() == output.size());
+	REQUIRE(expectedResult[0].first == output[0].first);
+	REQUIRE(expectedResult[0].second == output[0].second);
+
+	// SolveBoth(Modifies, Stmt, Print) for Modifies(1, 'a') returns {}
+	leftEntityName = EntityName::STMT;
+	rightEntityName = EntityName::PRINT;
+	expectedResult = {};
+	output = facade.solveBoth(RelationshipReference::MODIFIES, leftEntityName, rightEntityName);
+	REQUIRE(std::equal(expectedResult.begin(), expectedResult.end(), output.begin()));
+
+	// SolveBoth(Modifies, Call, Var) for Modifies(1, 'a') returns {}
+	leftEntityName = EntityName::CALL;
+	rightEntityName = EntityName::VARIABLE;
+	expectedResult = {};
+	output = facade.solveBoth(RelationshipReference::MODIFIES, leftEntityName, rightEntityName);
+	REQUIRE(std::equal(expectedResult.begin(), expectedResult.end(), output.begin()));
+}
+
+TEST_CASE("ProcToVar: Validate returns correct results") {
+	Storage storage;
+	QueryFacade facade = QueryFacade(&storage);
+	ProceduresTable* procedures = (ProceduresTable*)storage.getTable(TableName::PROCEDURES);
+	VariablesTable* variables = (VariablesTable*)storage.getTable(TableName::VARIABLES);
+	ModifiesPTable* modifiesP = (ModifiesPTable*)storage.getTable(TableName::MODIFIES_P);
+	
+	Procedure proc1 = Procedure("Main");
+	Variable var1 = Variable("a");
+	Relationship<std::string, std::string> rs = Relationship(
+		RelationshipReference::MODIFIES, std::string("Main"), std::string("a")
+	);
+	procedures->store(&proc1);
+	variables->store(&var1);
+	modifiesP->store(&rs);
+
+	Reference leftRef = Reference("Main");
+	Reference rightRef = Reference("a");
+	Reference wildcardRef = Reference("_");
+
+	// Queries where relationships do exist
+	REQUIRE(facade.validate(RelationshipReference::MODIFIES, leftRef, rightRef));
+	REQUIRE(facade.validate(RelationshipReference::MODIFIES, leftRef, wildcardRef));
+	REQUIRE(facade.validate(RelationshipReference::MODIFIES, wildcardRef, rightRef));
+	REQUIRE(facade.validate(RelationshipReference::MODIFIES, wildcardRef, wildcardRef));
+
+	// Queries where relationships do not exist
+	REQUIRE(!facade.validate(RelationshipReference::MODIFIES, Reference("Foo"), rightRef));
+	REQUIRE(!facade.validate(RelationshipReference::MODIFIES, leftRef, Reference("x")));
+	REQUIRE(!facade.validate(RelationshipReference::MODIFIES, Reference("Foo"), wildcardRef));
+	REQUIRE(!facade.validate(RelationshipReference::MODIFIES, wildcardRef, Reference("x")));
+}
+
+TEST_CASE("ProcToVar: SolveRight queries for Modifies('Main', 'a') return correct results") {
+	Storage storage;
+	QueryFacade facade = QueryFacade(&storage);
+	ProceduresTable* procedures = (ProceduresTable*)storage.getTable(TableName::PROCEDURES);
+	VariablesTable* variables = (VariablesTable*)storage.getTable(TableName::VARIABLES);
+	ModifiesPTable* modifiesP = (ModifiesPTable*)storage.getTable(TableName::MODIFIES_P);
+	
+	Procedure proc1 = Procedure("Main");
+	Variable var1 = Variable("a");
+	Relationship<std::string, std::string> rs = Relationship(
+		RelationshipReference::MODIFIES, std::string("Main"), std::string("a")
+	);
+	procedures->store(&proc1);
+	variables->store(&var1);
+	modifiesP->store(&rs);
+
+	Reference leftRef;
+	EntityName rightEntityName;
+	std::vector<Value> expectedResult;
+	std::vector<Value> output;
+	
+	// SolveRight(Modifies, Main, Variable) for Modifies('Main', 'a') returns {'a'}
+	leftRef = Reference("Main");
+	rightEntityName = EntityName::VARIABLE;
+	expectedResult = {Value(ValueType::VAR_NAME, "a")};
+	output = facade.solveRight(RelationshipReference::MODIFIES, leftRef, rightEntityName);
+	REQUIRE(std::equal(expectedResult.begin(), expectedResult.end(), output.begin()));
+
+	// SolveRight(Modifies, Foo, Variable) for Modifies('Main', 'a') returns {}
+	leftRef = Reference("Foo");
+	rightEntityName = EntityName::VARIABLE;
+	expectedResult = {};
+	output = facade.solveRight(RelationshipReference::MODIFIES, leftRef, rightEntityName);
+	REQUIRE(std::equal(expectedResult.begin(), expectedResult.end(), output.begin()));
+
+	// SolveRight(Modifies, Main, Procedure) for Modifies('Main', 'a') returns {}
+	leftRef = Reference("Main");
+	rightEntityName = EntityName::PROCEDURE;
+	expectedResult = {};
+	output = facade.solveRight(RelationshipReference::MODIFIES, leftRef, rightEntityName);
+	REQUIRE(std::equal(expectedResult.begin(), expectedResult.end(), output.begin()));
+
+	// SolveRight(Modifies, _, Variable) for Modifies('Main', 'a') returns {'a'}
+	leftRef = Reference("_");
+	rightEntityName = EntityName::VARIABLE;
+	expectedResult = {Value(ValueType::VAR_NAME, "a")};
+	output = facade.solveRight(RelationshipReference::MODIFIES, leftRef, rightEntityName);
+	REQUIRE(std::equal(expectedResult.begin(), expectedResult.end(), output.begin()));
+}
+
+TEST_CASE("ProcToVar: SolveLeft queries for Modifies('Main', 'a') return correct results") {
+	Storage storage;
+	QueryFacade facade = QueryFacade(&storage);
+	ProceduresTable* procedures = (ProceduresTable*)storage.getTable(TableName::PROCEDURES);
+	VariablesTable* variables = (VariablesTable*)storage.getTable(TableName::VARIABLES);
+	ModifiesPTable* modifiesP = (ModifiesPTable*)storage.getTable(TableName::MODIFIES_P);
+	
+	Procedure proc1 = Procedure("Main");
+	Variable var1 = Variable("a");
+	Relationship<std::string, std::string> rs = Relationship(
+		RelationshipReference::MODIFIES, std::string("Main"), std::string("a")
+	);
+	procedures->store(&proc1);
+	variables->store(&var1);
+	modifiesP->store(&rs);
+
+	Reference rightRef;
+	EntityName leftEntityName;
+	std::vector<Value> expectedResult;
+	std::vector<Value> output;
+
+	// SolveLeft(Modifies, a, Procedure) for Modifies('Main', 'a') returns {'Main'}
+	rightRef = Reference("a");
+	leftEntityName = EntityName::PROCEDURE;
+	expectedResult = {Value(ValueType::VAR_NAME, "Main")};
+	output = facade.solveLeft(RelationshipReference::MODIFIES, rightRef, leftEntityName);
+	REQUIRE(std::equal(expectedResult.begin(), expectedResult.end(), output.begin()));
+
+	// SolveLeft(Modifies, x, Procedure) for Modifies('Main', 'a') returns {}
+	rightRef = Reference("x");
+	leftEntityName = EntityName::PROCEDURE;
+	expectedResult = {};
+	output = facade.solveLeft(RelationshipReference::MODIFIES, rightRef, leftEntityName);
+	REQUIRE(std::equal(expectedResult.begin(), expectedResult.end(), output.begin()));
+
+	// SolveLeft(Modifies, a, Statement) for Modifies('Main', 'a') returns {}
+	rightRef = Reference("a");
+	leftEntityName = EntityName::STMT;
+	expectedResult = {};
+	output = facade.solveLeft(RelationshipReference::MODIFIES, rightRef, leftEntityName);
+	REQUIRE(std::equal(expectedResult.begin(), expectedResult.end(), output.begin()));
+
+	// SolveLeft(Modifies, _, Procedure) for Modifies('Main', 'a') returns {'Main'}
+	rightRef = Reference("_");
+	leftEntityName = EntityName::PROCEDURE;
+	expectedResult = {Value(ValueType::VAR_NAME, "Main")};
+	output = facade.solveLeft(RelationshipReference::MODIFIES, rightRef, leftEntityName);
+	REQUIRE(std::equal(expectedResult.begin(), expectedResult.end(), output.begin()));
+
+}
+
+TEST_CASE("ProcToVar: SolveBoth queries for Modifies('Main', 'a') return correct results") {
+	Storage storage;
+	QueryFacade facade = QueryFacade(&storage);
+	ProceduresTable* procedures = (ProceduresTable*)storage.getTable(TableName::PROCEDURES);
+	VariablesTable* variables = (VariablesTable*)storage.getTable(TableName::VARIABLES);
+	ModifiesPTable* modifiesP = (ModifiesPTable*)storage.getTable(TableName::MODIFIES_P);
+	
+	Procedure proc1 = Procedure("Main");
+	Variable var1 = Variable("a");
+	Relationship<std::string, std::string> rs = Relationship(
+		RelationshipReference::MODIFIES, std::string("Main"), std::string("a")
+	);
+	procedures->store(&proc1);
+	variables->store(&var1);
+	modifiesP->store(&rs);
+	Value value1 = Value(ValueType::VAR_NAME, "Main");
+	Value value2 = Value(ValueType::VAR_NAME, "a");
+
+	EntityName leftEntityName;
+	EntityName rightEntityName;
+	std::vector<std::pair<Value,Value>> expectedResult;
+	std::vector<std::pair<Value,Value>> output;
+
+	// SolveBoth(Modifies, Proc, Var) for Modifies('Main', 'a') returns {('1', '2')}
+	leftEntityName = EntityName::PROCEDURE;
+	rightEntityName = EntityName::VARIABLE;
+	expectedResult = {std::make_pair(value1, value2)};
+	output = facade.solveBoth(RelationshipReference::MODIFIES, leftEntityName, rightEntityName);
+	REQUIRE(expectedResult.size() == output.size());
+	REQUIRE(expectedResult[0].first == output[0].first);
+	REQUIRE(expectedResult[0].second == output[0].second);
+
+	// SolveBoth(Modifies, Proc, Print) for Modifies('Main', 'a') returns {}
+	leftEntityName = EntityName::PROCEDURE;
+	rightEntityName = EntityName::PRINT;
+	expectedResult = {};
+	output = facade.solveBoth(RelationshipReference::MODIFIES, leftEntityName, rightEntityName);
+	REQUIRE(std::equal(expectedResult.begin(), expectedResult.end(), output.begin()));
+
+	// SolveBoth(Modifies, Call, Var) for Modifies('Main', 'a') returns {}
+	leftEntityName = EntityName::CALL;
+	rightEntityName = EntityName::VARIABLE;
+	expectedResult = {};
+	output = facade.solveBoth(RelationshipReference::MODIFIES, leftEntityName, rightEntityName);
+	REQUIRE(std::equal(expectedResult.begin(), expectedResult.end(), output.begin()));
+}
