84dda9a94a8777a4669caecde1f6cba5ad971040
cnlinh <linh.caongoc12@gmail.com>
2022-09-16 11:55:05 +0800

    Add pattern evaluation logic

--- Team28/Code28/src/spa/src/QPS/Evaluator/PatternEvaluator.cpp
+++ Team28/Code28/src/spa/src/QPS/Evaluator/PatternEvaluator.cpp
@@ -5,6 +5,22 @@ ClauseResult PatternEvaluator::evaluate(PatternClause *patternCl) {
         return ClauseResult(false);
     }
     else if (patternCl->patternType == PatternType::ASSIGN) {
-        return ClauseResult(false);
+        if (patternCl->entRef.isSynonym) {
+            ClauseResult clauseResult = ClauseResult(std::vector{ patternCl->syn, patternCl->entRef.syn });
+            std::vector< std::pair<Value, Value> > result = queryFacade->getAssignAndVar(patternCl->expression);
+            for (int i = 0; i < result.size(); i++) {
+                clauseResult.insert(Tuple(std::vector{ result[i].first, result[i].second}));
+            }
+            return clauseResult;
+        }
+        else {
+            ClauseResult clauseResult = ClauseResult(std::vector{ patternCl->syn });
+            std::string varName = patternCl->entRef.value.value;
+            std::vector<Value> result = queryFacade->getAssign(varName, patternCl->expression);
+            for (int i = 0; i < result.size(); i++) {
+                clauseResult.insert(Tuple(std::vector{ result[i] }));
+            }
+            return clauseResult;
+        }
     }
 }
