c254a57500366f315e743d78eda0ef77c15aac32
luayida99 <77190041+luayida99@users.noreply.github.com>
2022-09-19 11:10:26 +0800

    Update autotester script, rename source code files

--- run-autotester.py
+++ run-autotester.py
@@ -1,8 +1,12 @@
+# Python script to run all Autotester tests.
+# To add new Autotester tests, add a source code file {name}_source.txt to Tests28/source
+# and a query file {name}_queries.txt to Tests28/source
+
 import sys
 import os
 
 def autotest(source_path):
-    cmd = "Team28//Code28//out//build//x64-Debug//src//autotester//autotester.exe Team28//Tests28//source//{}.txt Team28//Tests28//queries//{}_queries.txt Team28//Tests28//out_{}.xml".format(
+    cmd = "Team28//Code28//out//build//x64-Debug//src//autotester//autotester.exe Team28//Tests28//source//{}_source.txt Team28//Tests28//queries//{}_queries.txt Team28//Tests28//out_{}.xml".format(
         source_path, source_path, source_path
     )
     os.system(cmd)
@@ -16,7 +20,7 @@ if __name__ == "__main__":
     pathArr = os.listdir("Team28//Tests28//source")
 
     for i in range(len(pathArr)):
-        pathArr[i] = remove_suffix(pathArr[i], ".txt")
+        pathArr[i] = remove_suffix(pathArr[i], "_source.txt")
 
     for path in pathArr:
         autotest(path)
