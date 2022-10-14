d5420bd9fcc9394c9b3105348908cc6e539037f7
benedictchuajj <benedictchuajj@gmail.com>
2022-10-14 19:09:41 +0800

    Add simpler unit test

--- Team28/Code28/src/spa/src/PKB/Algorithms/ControlFlowGraph.cpp
+++ Team28/Code28/src/spa/src/PKB/Algorithms/ControlFlowGraph.cpp
@@ -42,24 +42,25 @@ void ControlFlowGraph::populateNextT() {
         matrix[elem.first] = elem.second;
     }
 
-    for (int k = 1; k <= this->totalLines; k++) {
-        std::cout << "MIDDLE" << std::endl;
-        for (int i = 1; i <= this->totalLines; k++) {
-            for (int j = 1; j <= this->totalLines; k++) {
-                std::pair<int, int> curr = std::make_pair(i, j);
-                std::pair<int, int> left = std::make_pair(i, k);
-                std::pair<int, int> right = std::make_pair(k, j);
-                matrix[curr] = matrix[curr] || (matrix[left] && matrix[right]);
-                std::cout << "(" << i << ", " << j << "): " << matrix[curr]
-                          << std::endl;
-            }
-        }
-    }
+    // for (int k = 1; k <= this->totalLines; k++) {
+    //     std::cout << "MIDDLE" << std::endl;
+    //     for (int i = 1; i <= this->totalLines; k++) {
+    //         for (int j = 1; j <= this->totalLines; k++) {
+    //             std::pair<int, int> curr = std::make_pair(i, j);
+    //             std::pair<int, int> left = std::make_pair(i, k);
+    //             std::pair<int, int> right = std::make_pair(k, j);
+    //             matrix[curr] = matrix[curr] || (matrix[left] &&
+    //             matrix[right]); std::cout << "(" << i << ", " << j << "): "
+    //             << matrix[curr]
+    //                       << std::endl;
+    //         }
+    //     }
+    // }
 
-    for (const auto &elem : matrix) {
-        std::cout << "(" << elem.first.first << ", " << elem.first.second
-                  << "): " << elem.second << std::endl;
-    }
+    // for (const auto &elem : matrix) {
+    //     std::cout << "(" << elem.first.first << ", " << elem.first.second
+    //               << "): " << elem.second << std::endl;
+    // }
 };
 
 void ControlFlowGraph::DFS(int i) {
