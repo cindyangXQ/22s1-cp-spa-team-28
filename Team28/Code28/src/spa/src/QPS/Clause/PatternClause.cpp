41992662f6dcc1940cd788b566079363fc344a96
Galnerd <e0543848@u.nus.edu>
2022-11-06 16:17:05 +0800

    fix bug for assign and while pattern also

--- Team28/Code28/src/spa/src/QPS/Clause/PatternClause.cpp
+++ Team28/Code28/src/spa/src/QPS/Clause/PatternClause.cpp
@@ -7,9 +7,16 @@ bool PatternClause::getIsExact() { return this->isExact; }
 
 void PatternClause::parse(std::smatch matches, std::vector<Synonym> syns) {
     this->stmtRef = Reference::getReference(matches[2], syns);
-    if (stmtRef.getEntityName() == EntityName::IF &&
-        !std::regex_search(matches[6].str(), IF_PATTERN)) {
-        throw SyntaxError("Too many wildcards at the end of If pattern");
+    if (stmtRef.getEntityName() == EntityName::IF) {
+        if (!std::regex_search(matches[6].str(), IF_PATTERN)) {
+            throw SyntaxError("Too many wildcards at the end of If pattern");
+        }
+    } else if (stmtRef.getEntityName() == EntityName::ASSIGN ||
+               stmtRef.getEntityName() == EntityName::WHILE) {
+        if (!std::regex_search(matches[6].str(), ASSIGN_WHILE_PATTERN)) {
+            throw SyntaxError(
+                "Too many wildcards at the end of Assign or while pattern");
+        }
     }
     this->entRef = Reference::getReference(matches[3], syns);
     Expression expr = Utils::trimSpaces(matches[5]);
